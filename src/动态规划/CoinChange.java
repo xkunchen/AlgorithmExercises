e863df399f65828abfed22baa475592485d1fbbe
xkunchen <1210692926@qq.com>
2020-09-23 17:32:31 +0800

    9月23日第二次提交，练习了动态规划，贪心算法

--- src/动态规划/CoinChange.java
+++ src/动态规划/CoinChange.java
@@ -15,6 +15,8 @@ package 动态规划;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * 零钱兑换
@@ -87,8 +89,99 @@ public class CoinChange {
         }
         return dp[amount] > amount ? -1 : dp[amount];
     }
+    //广度优先遍历,有时间看一下
+    public int coinChange4(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[amount + 1];
+
+        visited[amount] = true;
+        queue.offer(amount);
+
+        // 排序是为了加快广度优先遍历过程中，对***面值的遍历，起到剪枝的效果
+        Arrays.sort(coins);
+
+        int step = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Integer head = queue.poll();
+                for (int coin : coins) {
+                    int next = head - coin;
+                    // 只要遇到 0，就找到了一个最短路径
+                    if (next == 0) {
+                        return step;
+                    }
+
+                    if (next < 0) {
+                        // 由于 coins 升序排序，后面的面值会越来越大，剪枝
+                        break;
+                    }
+
+                    if (!visited[next]) {
+                        queue.offer(next);
+                        // 添加到队列的时候，就应该立即设置为 true
+                        // 否则还会发生重复访问
+                        visited[next] = true;
+                    }
+                }
+            }
+            step++;
+        }
+        // 进入队列的顶点都出队，都没有看到 0 ，就表示凑不出***
+        return -1;
+    }
+    //有时间看一下，DFS + 剪枝
+    int ans=Integer.MAX_VALUE;
+    public int coinChange5(int[] coins, int amount) {
+        Arrays.sort(coins);
+        dfs(coins,coins.length-1,amount,0);
+        return ans==Integer.MAX_VALUE?-1:ans;
+    }
+    public void dfs(int[] coins,int index,int amount,int cnt){
+        if(index<0){
+            return;
+        }
+        for(int c=amount/coins[index];c>=0;c--){
+            int na=amount-c*coins[index];
+            int ncnt=cnt+c;
+            if(na==0){
+                ans=Math.min(ans,ncnt);
+                break;//剪枝1
+            }
+            if(ncnt+1>=ans){
+                break; //剪枝2
+            }
+            dfs(coins,index-1,na,ncnt);
+        }
+    }
+    //贪心算法
+    int res = Integer.MAX_VALUE;
+    public int coinChange6(int[] coins, int amount){
+        if(amount==0){
+            return 0;
+        }
+        Arrays.sort(coins);
+        mincoin(coins,amount,coins.length-1,0);
+        return res==Integer.MAX_VALUE? -1:res;
+    }
+    private void mincoin(int[] coins,int amount, int index, int count){
+        if(amount==0){
+            res = Math.min(res,count);
+            return;
+        }
+        if(index<0){
+            return;
+        }
+        for(int i = amount/coins[index];i>=0 && i+count<res; i--){
+            mincoin(coins,amount - (i*coins[index]), index-1, count+i);
+        }
+    }
     public static void main(String[] args) {
         CoinChange c=new CoinChange();
-        c.coinChange2( new int[]{1, 2, 5},11);
+        c.coinChange6( new int[]{3,4, 5},26);
     }
 }
