26e3a6cfd1bfaab56c7f038b3f6e5e27350313be
xkunchen <1210692926@qq.com>
2020-10-09 17:06:39 +0800

    10月9日第一次提交，练习了二分查找

--- src/二分查找/SearchA2dMatrix.java
+++ src/二分查找/SearchA2dMatrix.java
@@ -60,7 +60,25 @@ public class SearchA2dMatrix {
         }
         return  false;
     }
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        int m = matrix.length;
+        if (m == 0) return false;
+        int n = matrix[0].length;
 
+        // 二分查找
+        int left = 0, right = m * n - 1;
+        int pivotIdx, pivotElement;
+        while (left <= right) {
+            pivotIdx = (left + right) / 2;
+            pivotElement = matrix[pivotIdx / n][pivotIdx % n];
+            if (target == pivotElement) return true;
+            else {
+                if (target < pivotElement) right = pivotIdx - 1;
+                else left = pivotIdx + 1;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         SearchA2dMatrix s=new SearchA2dMatrix();
         //int[][] arr=new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,50},{51,54,78,89}};
