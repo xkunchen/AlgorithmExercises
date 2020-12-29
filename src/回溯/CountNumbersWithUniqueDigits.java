ccad870c9429abaee8e78f5171f559ae3627c4d6
15915667912a <xkunchen>
2020-12-29 17:30:03 +0800

    12月29日第一次提交，练习了回溯算法

---
+++ src/回溯/CountNumbersWithUniqueDigits.java
@@ -0,0 +1,96 @@
+package 回溯;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//给定一个非负整数 n，计算各位数字都不同的数字 x 的个数，其中 0 ≤ x < 10n 。
+// 示例:
+// 输入: 2
+//输出: 91
+//解释: 答案应为除去 11,22,33,44,55,66,77,88,99 外，在 [0,100) 区间内的所有数字。
+// Related Topics 数学 动态规划 回溯算法
+// 👍 106 👎 0
+
+/**
+ * 计算各个位数不同的数字个数
+ *         https://leetcode-cn.com/problems/count-numbers-with-unique-digits
+ *         回溯
+ */
+public class CountNumbersWithUniqueDigits {
+    /**
+     * 分析，如何判断数字相等，
+     */
+    //暴力法
+    public int countNumbersWithUniqueDigits(int n) {
+        int result=0;
+        for (Integer i = 0; i < (int)Math.pow(10,n); i++) {
+            if (judge(i.toString().length(),i)){
+                result++;
+            }
+        }
+        return result;
+    }
+    //判断数字是否符合要求
+    private boolean judge(int n,int number){
+        if (n==1){
+            return true;
+        }
+        Map<Integer,Object> remainderMap=new HashMap<Integer, Object>();
+        int remainder=number;
+        int remainder1=0;
+        for (int i=n-1;i>=0;i--){
+            //除数
+            remainder1=remainder / (int) Math.pow(10,i);
+            //余数
+            remainder=remainder % (int) Math.pow(10,i);
+            if (remainderMap.containsKey(remainder1)){
+                return false;
+            }
+            remainderMap.put(remainder1,null);
+        }
+        return true;
+    }
+    //迭代法
+    public int countNumbersWithUniqueDigits2(int n) {
+        if (n==0){
+            return 1;
+        }
+        n=Math.min(n,10);
+        int ans = 10, base = 9, sum = 9;
+        for(int i = 1; i < n; ++i){
+            sum *= base;
+            ans += sum;
+            base--;
+        }
+        return ans;
+    }
+    //回溯
+    public int countNumbersWithUniqueDigits3(int n) {
+        if (n == 0) return 1;
+        return dfs(Math.min(10, n), 0, new boolean[10]);
+    }
+
+    private int dfs(int n, int idx, boolean[] used) {
+        int count = 0;
+        if (idx != n) {
+            for (int i = 0; i < 10; i++) {
+                // 剪枝：多位数时，第一个数字不能为0
+                if (i == 0 && n > 1 && idx == 1) {
+                    continue;
+                }
+                // 剪枝：不能使用用过的数字
+                if (used[i]) {
+                    continue;
+                }
+                used[i] = true;
+                count += dfs(n, idx + 1, used) + 1;
+                used[i] = false;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        CountNumbersWithUniqueDigits c=new CountNumbersWithUniqueDigits();
+        System.out.println(c.countNumbersWithUniqueDigits3(3));
+    }
+}
--- src/回溯/NQueens.java
+++ src/回溯/NQueens.java
@@ -24,66 +24,101 @@ package 回溯;
 import java.util.*;
 
 /**
- *         N皇后
- *         https://leetcode-cn.com/problems/n-queens/
- *          回溯
- *          第一遍
+ * N皇后
+ * https://leetcode-cn.com/problems/n-queens/
+ * 回溯
+ * 第一遍
+ */
+
+/**
+ 理解：1.首先创建三个集合 columns diagonals1 diagonals2分别记录每一列以及两个方向的每条斜线上是否有皇后。
+ 条件：在斜边分别行加宽相等，还有行减宽相等
+ 2.回溯剪枝，最重要的是此路不通后的数据还原处理
  */
 public class NQueens {
-        public List<List<String>> solveNQueens(int n) {
-            List<List<String>> solutions = new ArrayList<List<String>>();
-            int[] queens = new int[n];
-            Arrays.fill(queens, -1);
-            Set<Integer> columns = new HashSet<Integer>();
-            Set<Integer> diagonals1 = new HashSet<Integer>();
-            Set<Integer> diagonals2 = new HashSet<Integer>();
-            backtrack(solutions, queens, n, 0, columns, diagonals1, diagonals2);
-            return solutions;
-        }
-        // 结果，，皇后数，
-        public void backtrack(List<List<String>> solutions, int[] queens, int n, int row, Set<Integer> columns, Set<Integer> diagonals1, Set<Integer> diagonals2) {
-            if (row == n) {
-                List<String> board = generateBoard(queens, n);
-                solutions.add(board);
-            } else {
-                for (int i = 0; i < n; i++) {
-                    if (columns.contains(i)) {
-                        continue;
-                    }
-                    int diagonal1 = row - i;
-                    if (diagonals1.contains(diagonal1)) {
-                        continue;
-                    }
-                    int diagonal2 = row + i;
-                    if (diagonals2.contains(diagonal2)) {
-                        continue;
-                    }
-                    queens[row] = i;
-                    columns.add(i);
-                    diagonals1.add(diagonal1);
-                    diagonals2.add(diagonal2);
-                    backtrack(solutions, queens, n, row + 1, columns, diagonals1, diagonals2);
-                    queens[row] = -1;
-                    columns.remove(i);
-                    diagonals1.remove(diagonal1);
-                    diagonals2.remove(diagonal2);
-                }
-            }
-        }
+    //三个集合 columns diagonals1 diagonals2
+    //  分别记录每一列以及两个方向的每条斜线上是否有皇后。
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> solutions = new ArrayList<List<String>>();
+        int[] queens = new int[n];
+        Arrays.fill(queens, -1);
+        Set<Integer> columns = new HashSet<Integer>();
+        Set<Integer> diagonals1 = new HashSet<Integer>();
+        Set<Integer> diagonals2 = new HashSet<Integer>();
+        backtrack(solutions, queens, n, 0, columns, diagonals1, diagonals2);
+        return solutions;
+    }
 
-        public List<String> generateBoard(int[] queens, int n) {
-            List<String> board = new ArrayList<String>();
+    // 结果，当前皇后的位置，皇后数，当前第几行，三个表示每一列以及两个方向的每条斜线上是否有皇后
+    public void backtrack(List<List<String>> solutions, int[] queens, int n, int row, Set<Integer> columns, Set<Integer> diagonals1, Set<Integer> diagonals2) {
+        if (row == n) {
+            List<String> board = generateBoard(queens, n);
+            solutions.add(board);
+        } else {
             for (int i = 0; i < n; i++) {
-                char[] row = new char[n];
-                Arrays.fill(row, '.');
-                row[queens[i]] = 'Q';
-                board.add(new String(row));
+                //如果列有皇后
+                if (columns.contains(i)) {
+                    continue;
+                }
+                int diagonal1 = row - i;
+                if (diagonals1.contains(diagonal1)) {
+                    continue;
+                }
+                int diagonal2 = row + i;
+                if (diagonals2.contains(diagonal2)) {
+                    continue;
+                }
+                queens[row] = i;
+                columns.add(i);
+                diagonals1.add(diagonal1);
+                diagonals2.add(diagonal2);
+                backtrack(solutions, queens, n, row + 1, columns, diagonals1, diagonals2);
+                queens[row] = -1;
+                columns.remove(i);
+                diagonals1.remove(diagonal1);
+                diagonals2.remove(diagonal2);
             }
-            return board;
         }
+    }
+
+    public List<String> generateBoard(int[] queens, int n) {
+        List<String> board = new ArrayList<String>();
+        for (int i = 0; i < n; i++) {
+            char[] row = new char[n];
+            Arrays.fill(row, '.');
+            row[queens[i]] = 'Q';
+            board.add(new String(row));
+        }
+        return board;
+    }
+
+    public List<List<String>> solveNQueens2(int n) {
+        int[] queens = new int[n];
+        Arrays.fill(queens, -1);
+        List<List<String>> solutions = new ArrayList<List<String>>();
+        solve(solutions, queens, n, 0, 0, 0, 0);
+        return solutions;
+    }
+
+    public void solve(List<List<String>> solutions, int[] queens, int n, int row, int columns, int diagonals1, int diagonals2) {
+        if (row == n) {
+            List<String> board = generateBoard(queens, n);
+            solutions.add(board);
+        } else {
+            int availablePositions = ((1 << n) - 1) & (~(columns | diagonals1 | diagonals2));
+            while (availablePositions != 0) {
+                int position = availablePositions & (-availablePositions);
+                availablePositions = availablePositions & (availablePositions - 1);
+                int column = Integer.bitCount(position - 1);
+                queens[row] = column;
+                solve(solutions, queens, n, row + 1, columns | position, (diagonals1 | position) << 1, (diagonals2 | position) >> 1);
+                queens[row] = -1;
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        NQueens n=new NQueens();
-        n.solveNQueens(4);
+        NQueens n = new NQueens();
+        n.solveNQueens2(4);
     }
 }
